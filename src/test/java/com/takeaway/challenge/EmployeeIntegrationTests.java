0e71c25a2f969276929e0b4fffb692df0ad6f528
Kumashi <naveen.kumashi@dxc.com>
2020-12-09 11:44:39 +0100

    Added integration tests for Employee entity.

--- src/test/java/com/takeaway/challenge/EmployeeIntegrationTests.java
+++ src/test/java/com/takeaway/challenge/EmployeeIntegrationTests.java
@@ -4,6 +4,8 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import java.time.LocalDate;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -11,13 +13,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.json.JacksonTester;
-import org.springframework.http.HttpHeaders;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.util.Base64Utils;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.takeaway.challenge.vo.DepartmentRequestParameters;
+import com.takeaway.challenge.vo.EmployeeRequestParameters;
 
 /**
  * @author Naveen Kumashi
@@ -26,11 +26,11 @@ import com.takeaway.challenge.vo.DepartmentRequestParameters;
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
 @AutoConfigureMockMvc
-public class DepartmentIntegrationTests {
+public class EmployeeIntegrationTests {
 	@Autowired
 	private MockMvc mockMvc;
 	
-	private JacksonTester<DepartmentRequestParameters> json;
+	private JacksonTester<EmployeeRequestParameters> employeeJSON;
 	
 	@BeforeEach
     public void setup() {
@@ -38,34 +38,24 @@ public class DepartmentIntegrationTests {
     }
 	
 	@Test
-	void should_fail_creating_a_department_no_auth_provided() throws Exception {		
-		DepartmentRequestParameters params = new DepartmentRequestParameters();
+	void should_fail_creating_an_employee_no_auth_provided() throws Exception {		
+		EmployeeRequestParameters params = new EmployeeRequestParameters();
 		params.setDepartmentId(1l);
 		params.setDepartmentName("Dummy");
+		params.setEmail("dummy@email.com");
+		params.setFirstName("First name");
+		params.setLastName("Last name");
+		params.setDob(LocalDate.now());
 	 		
-		mockMvc.perform(post("/api/department")
+		mockMvc.perform(post("/api/employee")
 	            .contentType("application/json")
-	            .content(json.write(params).getJson()))
+	            .content(employeeJSON.write(params).getJson()))
 	            .andExpect(status().is4xxClientError());     
 	}
 	
 	@Test
-	void should_succeed_creating_a_department_auth_provided() throws Exception {		
-		DepartmentRequestParameters params = new DepartmentRequestParameters();
-		params.setDepartmentId(1l);
-		params.setDepartmentName("Dummy");
-	 		
-		mockMvc.perform(post("/api/department")
-				.header(HttpHeaders.AUTHORIZATION,
-	                    "Basic " + Base64Utils.encodeToString("admin:admin123".getBytes()))
-	            .contentType("application/json")
-	            .content(json.write(params).getJson()))
-	            .andExpect(status().is2xxSuccessful());     
-	}
-	
-	@Test
-	void should_fetch_departments() throws Exception {			 	
-		mockMvc.perform(get("/api/departments")
+	void should_fetch_employees() throws Exception {					 	
+		mockMvc.perform(get("/api/employees")
 	            .contentType("application/json"))
 	            .andExpect(status().isOk());   
 	}
